a74dc39ed167676c073ac764f6ee42fa94f6fc25
kellisarre <kelli.sarre@gmail.com>
2019-01-07 11:49:05 -0600

    changed test to expect boolean value for getElementsByClassName; getElementsByClassName is passing local test

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  let returnedElements = [];
+    function getClasses (currNode) {
+        let children = currNode.childNodes;
+        console.log(currNode.classList);
+        // check for a classList && if classList contains <className>
+        if (currNode.classList && currNode.classList.contains(className)) {
+            // push current element into the returnedElements array            
+            returnedElements.push(currNode);
+        }
+        // if currNode has child node, recurse
+        if (children) {
+            for (let i = 0; i < children.length; i++) {
+                getClasses(children[i]);
+            }
+        }
+    }
+    getClasses(document.body);
+    return returnedElements;
 };
