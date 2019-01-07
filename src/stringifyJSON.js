b1f93a8cf93a688e2dee174d229f44d6e6c2e824
Michael LeMaire Jr <spark.michael.lemaire@gmail.com>
2019-01-07 12:31:33 -0600

    added case for object and array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,5 +5,25 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-  
+  let result = '';
+  function identifier(value){
+    switch (typeof value) {
+      case "boolean": 
+      case "string": 
+      case "number": 
+        return `${value}`;
+      case 'undefined':
+      case 'function': 
+        return "null";
+      case 'object':
+        if(Array.isArray(value)){
+          //if the given value is an array add a open array bracket
+          let arrResult = '['
+          for(let i = 0; i < value.length ; i++){
+
+          }
+        }
+    }
+  }
+  return identifier(obj);
 };
